43679415a17eaf3d8a80541007bf5cfff8d7be27
edward313 <56766386+edward313@users.noreply.github.com>
2021-12-14 21:50:23 +0700

    cap nhat cuoi cung

--- aws_trans_be_DTDM/router/translate.js
+++ aws_trans_be_DTDM/router/translate.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var AWS = require('aws-sdk');
 
 
-// var translate = new AWS.Translate({
-//   region: "us-east-2",
-//   accessKeyId: "AKIAR5RT323L2VIAO5FB",
-//   secretAccessKey: "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",
-//   });
+
+
 
 const config = require("../config.json");
 var express = require('express');
 var router = express.Router();
+
+
+// aws-sdk
+var AWS = require('aws-sdk');
 const REGION = config.REGION;
 const ACCESS_KEY = config.AWS_ACCESS_KEY;
 const SECRET_KEY = config.AWS_SECRET_KEY;
@@ -25,32 +25,15 @@ AWS.config.update({
 
 
 var translate = new AWS.Translate();
-const comprehend = new AWS.Comprehend();
+
+
+
 router.get('/', (req, res) => {
-  // res.status(200).send({message: "Luu Tan Phat!"});
+ 
   res.send("Ket noi backend")
 });
 
 
-// router.post('/translate', function(req, res, next) {
-//   var params = {
-//     SourceLanguageCode: req.body.SourceLanguageCode, /* required */
-//     TargetLanguageCode: req.body.TargetLanguageCode, /* required */
-//     Text: req.body.Text, /* required */
-//   }
-//   console.log(params.SourceLanguageCode)
-//   console.log(params)
-
-
-
-//   translate.translateText(params, function(err, data) {
-//     if (err) console.log(err, err.stack); // an error occurred
-//     else     {
-//       // console.log(data);
-//       res.status(200).send(data);
-      
-//     } 
-//   });
   
 router.post('/translate', function(req, res, next) {
   var params = {
@@ -58,10 +41,6 @@ router.post('/translate', function(req, res, next) {
     TargetLanguageCode: req.body.TargetLanguageCode, /* required */
     Text: req.body.Text, /* required */
   }
-  // comprehendResult =comprehend.detectDominantLanguage({ Text });
- 
-
-
   translate.translateText(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     {
